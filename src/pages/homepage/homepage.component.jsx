9483e8015cae1e34afa3ec1386d139cc39f127a8
Bhavesh-Singh-Mertiya <26bhaveshsingh@gmail.com>
2020-09-28 01:07:35 +0530

    update our menu-items to navigate withRouter

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './homepage.styles.scss'
 import Directory from '../../component/directory/directory.component'
 
-const Homepage = () => (	
-	<div className="homepage">
-		<Directory />
-	</div>	
-)
+const Homepage = () => {
+	return(	
+		<div className="homepage">
+			<Directory />
+		</div>	
+	)	
+}
+
 
 export default Homepage
